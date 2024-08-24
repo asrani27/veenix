9b1817929f9088186f8f1321c2834204959dd3ec
asrani <asrani.27@gmail.com>
2024-08-25 06:57:02 +0800

    f

--- app/Http/Controllers/PostUserController.php
+++ app/Http/Controllers/PostUserController.php
@@ -57,7 +57,7 @@ class PostUserController extends Controller
 
             $image->resize(175, 260);
             $image->save($destinationPathThumbnail . $filename);
-            $namafile = config('app.url') . ':8000/storage/poster/' . $filename;
+            $namafile = config('app.url') . '/storage/poster/' . $filename;
         } else {
             $namafile = Post::findOrFail($id)->image;
         }
