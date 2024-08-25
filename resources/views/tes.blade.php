4dd8dc192b53ff9e189f4bf0c8c5778cf2b4b371
asrani <asrani.27@gmail.com>
2024-08-25 13:47:22 +0800

    d

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -9,6 +9,6 @@ iframe {
 @endpush
 @section('content')
 <div class="container">
-        <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" allowfullscreen="yes" src="https://vidsrc.cc/v2/embed/movie/385687" allow="autoplay; fullscreen" style="width: 100%; height: 100%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" allowfullscreen="yes" src="https://gdriveplayer.club/player/VzFlbUNTZTN0enRtZXR3dHN5QlJDNDhOTVVyRFZydlVhNCtzeTFBZ1NTQXF2elorSVYveXNJUERpUStDZlUyRVhTcnZMWW16dHBCdTBCOEJxZnR2cDlUZmdMUjVzejlBRTdvVFFqVEFObzFMaHY1cmZzUnl4TUFuV1hWUVowRFc=" allow="autoplay; fullscreen" style="width: 100%; height: 100%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 @endsection
