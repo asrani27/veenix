6b35444fc3ee4b04478a96fba5d1bd164d3d27c9
asrani <asrani.27@gmail.com>
2024-08-25 13:26:45 +0800

    f

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -1,4 +1,14 @@
 @extends('visit.app')
+@push('css')
+<style>
+iframe { 
+    width: 100%;
+    aspect-ratio: 16 / 9;
+  }
+</style>
+@endpush
 @section('content')
-<iframe title="player" scrolling="no" frameborder="0" marginwidth="0" allowfullscreen="yes" src="https://vidsrc.cc/v2/embed/movie/385687" allow="autoplay; fullscreen" style="width: 100%; height: 100%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<div class="container">
+        <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" allowfullscreen="yes" src="https://vidsrc.cc/v2/embed/movie/385687" allow="autoplay; fullscreen" style="width: 100%; height: 100%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 @endsection
