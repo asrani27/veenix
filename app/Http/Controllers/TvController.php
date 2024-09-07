20b8813fb512993faab70dc1170d8059739ab4fd
asrani <asrani.27@gmail.com>
2024-09-07 08:52:46 +0800

    f

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -160,8 +160,9 @@ class TvController extends Controller
             $dom = new DOM();
             @$dom->loadHTML($html);
             $xpath = new Xpath($dom);
+
             if ($xpath->query('//div[@class="entry-content entry-content-single"]//p')->length == 0) {
-                $data['description'] == null;
+                $data['description'] = null;
             } else {
                 $data['description'] = $xpath->query('//div[@class="entry-content entry-content-single"]//p')->item(0)->nodeValue;
             }
