36b0e89ddfe1d611b0bb5d191c72498ede90032e
asrani <asrani.27@gmail.com>
2025-04-02 22:16:41 +0800

    f

--- resources/views/detail_series.blade.php
+++ resources/views/detail_series.blade.php
@@ -57,8 +57,10 @@
     <div class="col-md-12">
       <div class="card">
         <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" src="{{$data->link_video}}"
-          allow="autoplay; fullscreen" style="width: 100%; height: 80%; overflow: hidden;"
-          referrerpolicy="no-referrer-when-downgrade"></iframe>
+          allow="autoplay; fullscreen; clipboard-write; encrypted-media; browsing-topics"
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+          style="width: 100%; height: 80%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
       </div>
 
       <div class="card card-primary">
