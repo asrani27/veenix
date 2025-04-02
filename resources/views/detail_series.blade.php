205cdbfd8197751e442e7976c9295a90b1536282
asrani <asrani.27@gmail.com>
2025-04-02 22:08:30 +0800

    f

--- resources/views/detail_series.blade.php
+++ resources/views/detail_series.blade.php
@@ -26,14 +26,15 @@
 
 
 <style>
-  iframe { 
-      width: 100%;
-      aspect-ratio: 16 / 9;
-    }
-    #uppy{
-      width: 100%;
-      height: 500px;
-    }
+  iframe {
+    width: 100%;
+    aspect-ratio: 16 / 9;
+  }
+
+  #uppy {
+    width: 100%;
+    height: 500px;
+  }
 </style>
 @endpush
 @section('content')
@@ -45,7 +46,7 @@
         <li class="breadcrumb-item"><a href="/">Home</a></li>
 
         @foreach (json_decode($data->tvseries->genre) as $item)
-            
+
         <li class="breadcrumb-item"><a href="/genre/{{$item}}">{{$item}}</a></li>
         @endforeach
         <li class="breadcrumb-item active">{{$data->tvseries->title}}, Episode : {{$data->title}}</li>
@@ -55,28 +56,32 @@
   <div class="row">
     <div class="col-md-12">
       <div class="card">
-          <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" allowfullscreen="yes" src="{{$data->link_video}}" allow="autoplay; fullscreen" style="width: 100%; height: 80%; overflow: hidden;" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="player" scrolling="no" frameborder="0" marginwidth="0" src="{{$data->link_video}}"
+          allow="autoplay; fullscreen" style="width: 100%; height: 80%; overflow: hidden;"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
-      
+
       <div class="card card-primary">
         <div class="card-header">
           <h3 class="card-title">SEMUA EPISODE</h3>
 
           <div class="card-tools">
-            
+
           </div>
           <!-- /.card-tools -->
         </div>
         <div class="card-body">
-            @foreach ($semuaEpisode as $key => $item)
-              @if ($item->id == $data->id)
-                  
-              <a href="/tv/{{$data->tvseries->slug}}/season-{{$item->season}}/episode-{{$item->episode}}"  class="btn btn-primary"><i class="fas fa-play"></i> S{{$item->season}} Ep {{$item->episode}}</a> 
-              @else
-                  
-              <a href="/tv/{{$data->tvseries->slug}}/season-{{$item->season}}/episode-{{$item->episode}}"  class="btn btn-default"><i class="fas fa-play"></i> S{{$item->season}} Ep {{$item->episode}}</a> 
-              @endif
-            @endforeach
+          @foreach ($semuaEpisode as $key => $item)
+          @if ($item->id == $data->id)
+
+          <a href="/tv/{{$data->tvseries->slug}}/season-{{$item->season}}/episode-{{$item->episode}}"
+            class="btn btn-primary"><i class="fas fa-play"></i> S{{$item->season}} Ep {{$item->episode}}</a>
+          @else
+
+          <a href="/tv/{{$data->tvseries->slug}}/season-{{$item->season}}/episode-{{$item->episode}}"
+            class="btn btn-default"><i class="fas fa-play"></i> S{{$item->season}} Ep {{$item->episode}}</a>
+          @endif
+          @endforeach
         </div>
       </div>
 
@@ -85,15 +90,16 @@
           <h3 class="card-title">LINK DOWNLOAD</h3>
 
           <div class="card-tools">
-            
+
           </div>
           <!-- /.card-tools -->
         </div>
         <div class="card-body">
           @if ($data->link_download != null)
-            @foreach (json_decode($data->link_download) as $key => $item)
-              <a href="{{$item}}"  class="btn btn-primary" target="_blank"><i class="fas fa-download"></i> Download {{$key + 1}}</a> 
-            @endforeach
+          @foreach (json_decode($data->link_download) as $key => $item)
+          <a href="{{$item}}" class="btn btn-primary" target="_blank"><i class="fas fa-download"></i> Download {{$key +
+            1}}</a>
+          @endforeach
           @endif
         </div>
       </div>
@@ -107,40 +113,40 @@
             <div class="col-sm-7" style="padding-left:25px">
               <h4><strong>{{$data->tvseries->title}} : {{$data->title}} </strong></h4>
               {{$data->description}}
-              <br/><br/>
-              <strong>Views: </strong>{{$data->views}}<br/>
+              <br /><br />
+              <strong>Views: </strong>{{$data->views}}<br />
 
               <strong>Genre: </strong>
               @if ($data->tvseries->genre != null)
-              
-                @foreach (json_decode($data->tvseries->genre) as $item)
-                    <a href="/genre/{{$item}}">{{$item}}</a>, 
-                @endforeach
-              @endif
-              <br/>
 
-              <strong>Director: </strong>{{$data->tvseries->director}}<br/>
+              @foreach (json_decode($data->tvseries->genre) as $item)
+              <a href="/genre/{{$item}}">{{$item}}</a>,
+              @endforeach
+              @endif
+              <br />
+
+              <strong>Director: </strong>{{$data->tvseries->director}}<br />
 
               <strong>Actors: </strong>
               @if ($data->tvseries->actor != null)
-                @foreach (json_decode($data->tvseries->actor) as $item)
-                {{$item}}, 
-                @endforeach
-                <br/>
+              @foreach (json_decode($data->tvseries->actor) as $item)
+              {{$item}},
+              @endforeach
+              <br />
               @endif
 
               <strong>Country: </strong>
               @if ($data->tvseries->country != null)
-                @foreach (json_decode($data->tvseries->country) as $item)
-                    {{$item}}, 
-                @endforeach<br/>
+              @foreach (json_decode($data->tvseries->country) as $item)
+              {{$item}},
+              @endforeach<br />
               @endif
-              <strong>Duration: </strong> {{$data->tvseries->duration}}<br/>
+              <strong>Duration: </strong> {{$data->tvseries->duration}}<br />
 
-              <strong>Release: </strong> {{$data->tvseries->release}}<br/>
+              <strong>Release: </strong> {{$data->tvseries->release}}<br />
 
-              <strong>IMDb: </strong>{{$data->tvseries->imdb == null ? 'N/A': $data->tvseries->imdb}}<br/>
-              <strong>Quality: </strong> {{$data->tvseries->quality}}<br/>
+              <strong>IMDb: </strong>{{$data->tvseries->imdb == null ? 'N/A': $data->tvseries->imdb}}<br />
+              <strong>Quality: </strong> {{$data->tvseries->quality}}<br />
             </div>
             <div class="col-sm-3 text-center">
               <script type="text/javascript">
@@ -152,16 +158,17 @@
                   'params' : {}
                 };
               </script>
-              <script type="text/javascript" src="//www.topcreativeformat.com/55687ce0e3fb33e799931945d583fc70/invoke.js"></script>
+              <script type="text/javascript"
+                src="//www.topcreativeformat.com/55687ce0e3fb33e799931945d583fc70/invoke.js"></script>
             </div>
           </div>
-          
+
         </div>
       </div>
     </div><!-- /.col -->
-    
+
   </div>
-  
+
   <div class="row">
     <div class="col-lg-12">
       <div class="card">
@@ -173,7 +180,7 @@
   </div>
 
 </div>
-    
+
 
 {{-- <div id="uppy">uppy</div> --}}
 @endsection
@@ -188,5 +195,6 @@
   (d.head || d.body).appendChild(s);
   })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+    Disqus.</a></noscript>
 @endpush
