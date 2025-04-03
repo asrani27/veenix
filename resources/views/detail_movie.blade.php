55ef1a0844f90d8cc60425c9aa3ecbdf396ff9c8
asrani <asrani.27@gmail.com>
2025-04-03 12:36:23 +0800

    f

--- resources/views/detail_movie.blade.php
+++ resources/views/detail_movie.blade.php
@@ -175,19 +175,8 @@
 </div>
 
 
-{{-- <div id="uppy">uppy</div> --}}
 @endsection
 
 @push('js')
 
-<script>
-  (function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://veenix-online.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-  })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
-    Disqus.</a></noscript>
 @endpush
