2c453a16888358f1eec0799487c2bb3ee60ec86e
asrani <asrani.27@gmail.com>
2025-04-03 22:01:48 +0800

    f

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -23,16 +23,17 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 @endpush
 @push('css')
-    
+
 <style>
-  iframe { 
-      width: 100%;
-      aspect-ratio: 16 / 9;
-    }
-    #uppy{
-      width: 100%;
-      height: 500px;
-    }
+  iframe {
+    width: 100%;
+    aspect-ratio: 16 / 9;
+  }
+
+  #uppy {
+    width: 100%;
+    height: 500px;
+  }
 </style>
 @endpush
 @section('content')
@@ -43,8 +44,8 @@
       <div class="card">
         <div class="card-body text-center">
           <img src="https://img.pikbest.com/origin/09/18/35/79ApIkbEsT3NC.png!sw800" width="20%">
-          <h2>BAGI KALIAN YANG MAU REQUEST FILM, <br/>SILAHKAN ISI KOMENTAR DI BAWAH INI :</h2> 
-          <i class="fa fa-8x fa-arrow-down"></i><br/><br/>
+          <h2>BAGI KALIAN YANG MAU REQUEST FILM, <br />SILAHKAN ISI KOMENTAR DI BAWAH INI :</h2>
+          <i class="fa fa-8x fa-arrow-down"></i><br /><br />
           <div id="disqus_thread"></div>
         </div>
       </div>
@@ -52,13 +53,12 @@
   </div>
 
 </div>
-    
+
 
 {{-- <div id="uppy">uppy</div> --}}
 @endsection
 
 @push('js')
-
 <script>
   (function() { // DON'T EDIT BELOW THIS LINE
   var d = document, s = d.createElement('script');
@@ -67,5 +67,6 @@
   (d.head || d.body).appendChild(s);
   })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+    Disqus.</a></noscript>
 @endpush
